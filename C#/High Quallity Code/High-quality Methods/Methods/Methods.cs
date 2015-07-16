495532e9246507adc322ffc51e4bda8fdb9b1475
Valentin Rangelov <eneragee@gmail.com>
2015-07-16 08:50:44 +0300

    Adding Homework High Quality Code/High-quality Methods

---
+++ C#/High Quallity Code/High-quality Methods/Methods/Methods.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new Exception("Sides cannot be negative, its not just possible!");
+            }
+            if ((a + b < c) || (a + c < b) || (b + c < a))
+            {
+                throw new Exception("The sum of every two sides must be bigger than the third one ! Law! ");
+            }
+
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        static string NumberToDigit(int number)
+        {
+            string numToDigit;
+            switch (number)
+            {
+                case 0: numToDigit = "zero"; break;
+                case 1: numToDigit = "one"; break;
+                case 2: numToDigit = "two"; break;
+                case 3: numToDigit = "three"; break;
+                case 4: numToDigit = "four"; break;
+                case 5: numToDigit = "five"; break;
+                case 6: numToDigit = "six"; break;
+                case 7: numToDigit = "seven"; break;
+                case 8: numToDigit = "eight"; break;
+                case 9: numToDigit = "nine"; break;
+                default: numToDigit = "Invalid number!"; break;
+            }
+
+            return numToDigit;
+        }
+
+        static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new Exception("Cant find max of empty array or null");
+            }
+
+            var maxNum = elements[0];
+            foreach (var num in elements)
+            {
+                if (maxNum < num)
+                {
+                    maxNum = num;
+                }
+            }
+            return maxNum;
+        }
+
+        static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+            if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+            if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+        }
+
+
+        static double CalcDistance(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = (y1 == y2);
+            isVertical = (x1 == x2);
+
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMax(5, -1, 3, 2, 14, 2, 3));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            Student peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
